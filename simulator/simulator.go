01308a920b9efebe27915d99a1c23c92e26f8330
Niels Hoffmann <niels@zentralmaschine.net>
2020-03-29 01:02:50 +0100

    Make Simulator use the new representation

--- simulator/simulator.go
+++ simulator/simulator.go
@@ -3,89 +3,67 @@ package simulator
 import (
 	"bytes"
 	"fmt"
-	"log"
 
-	"github.com/llgcode/draw2d/draw2dimg"
 	"github.com/nhoffmann/life/grid"
+	"github.com/nhoffmann/life/renderer"
 )
 
 type Simulator struct {
-	Rows            int
-	Columns         int
 	GenerationCount int
 	Rule            Rule
-	grid            *grid.Grid
+	Renderer        renderer.ImageRenderer
+	generation      *grid.Generation
 }
 
-func New(rows, columns int, ruleString string) *Simulator {
+func NewSimulator(ruleString string) *Simulator {
 	return &Simulator{
-		Rows:    rows,
-		Columns: columns,
-		Rule:    ParseRule(ruleString),
-		grid:    grid.New(rows, columns),
+		Rule:       ParseRule(ruleString),
+		generation: grid.NewGeneration(),
 	}
+}
+
+func NewSimulatorWithPattern(pattern [][]bool, ruleString string) *Simulator {
+	s := NewSimulator(ruleString)
+	s.generation.LoadPattern(pattern)
+	return s
+}
+
+func (s *Simulator) Generation() *grid.Generation {
+	return s.generation
 }
 
 // Evolute creates the next state of the grid and then replaces the current one
 func (s *Simulator) Evolute() {
-	newGrid := grid.New(s.Rows, s.Columns)
+	nextGeneration := grid.NewGeneration()
 
-	for rowIndex := 0; rowIndex < s.grid.Width(); rowIndex++ {
-		for columnIndex := 0; columnIndex < s.grid.Height(); columnIndex++ {
-			s.evoluteCell(newGrid, rowIndex, columnIndex)
-		}
+	for cell := range s.generation.CellMap {
+		s.evoluteMooreNeighborhood(nextGeneration, cell)
 	}
 
 	s.GenerationCount++
-	s.grid = newGrid
+	s.generation = nextGeneration
 }
 
-// LoadPattern populates the grid with a given pattern
-func (s *Simulator) LoadPattern(pattern [][]int) error {
-	patternWidth := len(pattern[0])
-	patternHeigth := len(pattern)
+func (s *Simulator) evoluteMooreNeighborhood(nextGeneration *grid.Generation, cell grid.Cell) {
+	s.evoluteCell(nextGeneration, cell)
 
-	startX := (s.grid.Width() - patternWidth) / 2
-	startY := (s.grid.Height() - patternHeigth) / 2
-
-	return s.LoadPatternAt(pattern, startX, startY)
-}
-
-func (s *Simulator) LoadPatternAt(pattern [][]int, startX, startY int) error {
-	patternWidth := len(pattern[0])
-	patternHeight := len(pattern)
-
-	if patternWidth > s.grid.Width() || patternHeight > s.grid.Height() {
-		log.Print(patternWidth >= s.grid.Width())
-		return fmt.Errorf("pattern too big for current grid")
-	}
-
-	for rowIndex, patternRow := range pattern {
-		for colIndex := range patternRow {
-			if pattern[rowIndex][colIndex] == 1 {
-				s.Populate(colIndex+startX, rowIndex+startY)
-			}
+	for _, neighboringCell := range cell.MooreNeighborhood() {
+		if !nextGeneration.IsPopulated(neighboringCell) {
+			s.evoluteCell(nextGeneration, neighboringCell)
 		}
 	}
-
-	return nil
 }
 
-// Populate populates the cell at the given coordinates
-func (s *Simulator) Populate(x, y int) {
-	s.grid.Populate(x, y)
-}
-
-func (s *Simulator) evoluteCell(grid *grid.Grid, rowIndex, columnIndex int) {
-	if s.cellLives(rowIndex, columnIndex) {
-		grid.Populate(rowIndex, columnIndex)
+func (s *Simulator) evoluteCell(nextGeneration *grid.Generation, cell grid.Cell) {
+	if s.cellLives(cell) {
+		nextGeneration.Populate(cell)
 	}
 }
 
-func (s *Simulator) cellLives(rowIndex, columnIndex int) bool {
-	count := s.grid.AliveNeigborCount(rowIndex, columnIndex)
+func (s *Simulator) cellLives(cell grid.Cell) bool {
+	count := s.generation.PopulatedNeighborsCount(cell)
 
-	if s.grid.IsPopulated(rowIndex, columnIndex) {
+	if s.generation.IsPopulated(cell) {
 		// apply survive rules
 		for _, surviveCount := range s.Rule.SurviveCounts {
 			if surviveCount == count {
@@ -109,12 +87,8 @@ func (s *Simulator) cellLives(rowIndex, columnIndex int) bool {
 func (s *Simulator) String() string {
 	var out bytes.Buffer
 
-	out.WriteString(s.grid.String())
+	// out.WriteString(s.generation.String())
 	fmt.Fprintf(&out, "Generation: %d", s.GenerationCount)
 
 	return out.String()
-}
-
-func (s *Simulator) Render(gc *draw2dimg.GraphicContext) bool {
-	return s.grid.Render(gc)
 }
